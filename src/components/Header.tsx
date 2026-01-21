6315f9eaf2ca9ebba15177e45a4ae53401a7b30c
qkrwns1478 <qkrwns1478@gmail.com>
2026-01-21 13:18:02 +0900

    chore: add new icon

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
 "use client";
 
-import { Rabbit } from "lucide-react";
+import Image from "next/image";
+import Logo from "../../public/icon.png"
 
 export default function Header() {
   return (
     <div className="text-center mb-8">
       <div className="inline-flex items-center justify-center p-3 bg-indigo-100 rounded-2xl mb-4">
-        <Rabbit className="w-8 h-8 text-indigo-600" />
+        <Image src={Logo} alt="Logo" width={48} height={48} />
       </div>
       <h1 className="text-3xl font-extrabold text-slate-800">
         Alice<span className="text-indigo-600">Lingo</span>
